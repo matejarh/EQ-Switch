677bcf22b26fc6149757e84c2a595e66db99fe7a
Matej Arh <matej.arh@gmail.com>
2025-08-01 18:20:59 +0200

    Feat: Adding g_SmallFontMono

--- src/utils/fonts.cpp
+++ src/utils/fonts.cpp
@@ -3,12 +3,15 @@
 
 // Global variable definition
 ImFont* g_SmallFont = nullptr;
+ImFont* g_SmallFontMono = nullptr;
 
 void LoadFonts(float baseFontSize)
 {
     ImGuiIO& io = ImGui::GetIO();
 
     io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\Carlito-Regular.ttf", baseFontSize);
+
+    g_SmallFontMono = io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\consola.ttf", 16.0f);
 
     g_SmallFont = io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\Carlito-Regular.ttf", 16.0f);
 
--- src/utils/fonts.h
+++ src/utils/fonts.h
@@ -4,6 +4,7 @@
 
 // Global pointer (or you can make it part of a class/namespace if preferred)
 extern ImFont* g_SmallFont;
+extern ImFont* g_SmallFontMono;
 
 // Call this once after ImGui context is created
 void LoadFonts(float baseFontSize = 20.0f);
