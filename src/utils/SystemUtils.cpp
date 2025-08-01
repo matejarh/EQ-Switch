657517e6710ec2d844c1929eb5089b9b20a05889
Matej Arh <matej.arh@gmail.com>
2025-08-01 21:44:14 +0200

    Feat: Adding IsProfilesFolderAccessible, IsLaunchEditorBatAvailable and IsEqualizerAPOInstalled functions

--- src/utils/SystemUtils.cpp
+++ src/utils/SystemUtils.cpp
@@ -5,3 +5,15 @@ bool IsEqualizerAPOInstalled()
     const std::wstring path = L"C:\\Program Files\\EqualizerAPO\\Editor.exe";
     return std::filesystem::exists(path);
 }
+
+bool IsProfilesFolderAccessible()
+{
+    const std::wstring path = L"..\\eq-presets\\";
+    return std::filesystem::exists(path);
+}
+
+bool IsLaunchEditorBatAvailable()
+{
+    const std::wstring path = L"launch_editor.bat";
+    return std::filesystem::exists(path);
+}
--- src/utils/SystemUtils.h
+++ src/utils/SystemUtils.h
@@ -1,3 +1,5 @@
 #pragma once
 
 bool IsEqualizerAPOInstalled();
+bool IsProfilesFolderAccessible();
+bool IsLaunchEditorBatAvailable();
