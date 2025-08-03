72d7848f69a3689c7d686e1aae992ff3501cc289
Matej Arh <matej.arh@gmail.com>
2025-08-03 14:50:30 +0200

    Fix: Clean up spaces

--- src/utils/VUMeters.h
+++ src/utils/VUMeters.h
@@ -16,9 +16,7 @@ class VuMeters
 {
 public:
     void VuMeterProgresBar(VUBuffer &vuBuffer, float main_scale);
-   
     void LedVuMeterHorizontal(VUBuffer &vuBuffer, float main_scale);
-    
     void LedVuMeter(VUBuffer &vuBuffer, float main_scale);
     
 };
